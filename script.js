8a722890a02fa3387943a159bcc7210caeb18378
Myung <god.godzilla.af@gmail.com>
2020-01-11 16:16:09 -0500

    calculator fully functional

--- script.js
+++ script.js
@@ -1,72 +1,96 @@
 
 
 //mathematical functions
-let add = (a,b) => a+b
-let subtract = (a,b) => a-b
-let multiply = (a,b) => a*b
-let divide = (a,b) => b != 0 ? a/b : "ERROR: div/0"
+let add = (a,b) => result = a+b
+let subtract = (a,b) => result = a-b
+let multiply = (a,b) => result = a*b
+let divide = (a,b) => b != 0 ? result = Math.round(a/b * 10) / 10 : "ERROR: div/0"
 let operate = (a, oper, b) => {
-	if (oper == 'add')
-		return(add(a,b));
-	if (oper == 'subtract')
-		return(subtract(a,b));
-	if (oper == 'multiply')
-		return(multiply(a,b));
-	if (oper == 'divide');
-		return(divide(a,b));
+	if (oper == 'add'){
+		add(a,b)
+		updateCalc()
+	}
+	if (oper == 'subtract'){
+		subtract(a,b)
+		updateCalc()
+	}
+	if (oper == 'multiply'){
+		multiply(a,b)
+		updateCalc()
+	}
+	if (oper == 'divide'){
+		divide(a,b)
+		updateCalc()
+	}
+	console.log(displayValue + " " + result)
+	
 }
 
 //num storage
 let display = document.querySelector("#display");
 let displayValue = 0;
-let a = 0;
-let oper = "";
-let b = 0;
-let tempResult = operate(oper, a, b);
 
 //display functions
-let updateDisplay = () => {
-	display.textContent = displayValue
-};
-/*
-let addDisplay = () => {displayValue != 0 ?
-	displayValue += document.getElementById(this.value) : displayValue = '';
-	console.log(numBtns.value + " addDisplay called")
+let updateDisplay = () => display.textContent = displayValue
+
+let updateCalc = () =>{
+	displayValue = result
+	console.log(result)
 	updateDisplay()
+	a = result
 }
-*/
+
 function addDisplay() {
 	if(displayValue == 0){
 		displayValue = ''
-		console.log("addDisplay called but no value input")
 	}
 	displayValue += this.value
-	console.log("addDisplay called AND " + this.value + " inputted");
+	updateDisplay()
 };
 
-let clearDisplay = () => (displayValue = 0, updateDisplay())
+let clearE = () => {
+	a = 0
+	b = 0
+	result = 0
+	oper = ''
+	clearDisplay()
+}
+
+let clearDisplay = () => (displayValue = '', updateDisplay())
 let delLast = () => {displayValue.length > 1 ?
 	displayValue = displayValue.slice(0,-1) : displayValue = 0;
 	updateDisplay()
 }
-/*
-let addOper = (a, oper) => {
+
+let addOper = () => {
 	a = displayValue;
-	oper = document.getElementByGroup('oper').value
 	clearDisplay()
 }
-*/
+
+let equal = () =>{
+	b = displayValue;
+	console.log(a + oper + b)
+	operate(Number(a), oper, Number(b));
+}
+
 
 //button event listeners
 const numBtns = document.querySelectorAll('.num');
 numBtns.forEach(button =>{
 	button.addEventListener('click', addDisplay);
-		console.log("numBtn clicked")
 });
 
-/*
+
 const operBtns = document.querySelectorAll('.oper');
 operBtns.forEach(button =>{
-	button.addEventListener('click', addOper);
+	button.addEventListener('click', (e) =>{
+		oper = button.id
+		addOper()
+	});
 });
-*/
+
+
+document.querySelector('#CE').addEventListener('click', clearE)
+document.querySelector('#C').addEventListener('click', clearDisplay)
+document.querySelector('#del').addEventListener('click', delLast)
+document.querySelector('#eq').addEventListener('click', equal)
