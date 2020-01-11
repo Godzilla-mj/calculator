88a469a486f7de4d2ad395cc28899bb69d855a67
Myung <god.godzilla.af@gmail.com>
2020-01-11 12:49:40 -0500

    functions formating changed - HTML - meta charset added

--- script.js
+++ script.js
@@ -4,31 +4,40 @@ let add = (a,b) => a+b
 let subtract = (a,b) => a-b
 let multiply = (a,b) => a*b
 let divide = (a,b) => b != 0 ? a/b : "ERROR: div/0"
-let operate = (a, oper, b) => (oper(a,b), updateDisplay())
+let operate = (a, oper, b) => {
+	if (oper == 'add')
+		return(add(a,b));
+	if (oper == 'subtract')
+		return(subtract(a,b));
+	if (oper == 'multiply')
+		return(multiply(a,b));
+	if (oper == 'divide');
+		return(divide(a,b));
+}
 //num storage
 let display = document.querySelector("#display");
 let displayValue = 0;
 let a = 0;
 let oper = "";
 let b = 0;
+console.log("test");
 let tempResult = operate(oper, a, b);
+console.log("test 2")
 
-debugger
 console.log("test");
 let updateDisplay = () => {
 	display.textContent = displayValue
 	console.log("updateDisplay");
 };
-let addDisplay = () => !0 
-	? displayValue += this.value 
-	: displayValue = ''
-	updateDisplay()
+let addDisplay = () => {!0 ?
+	(displayValue += this.value, updateDisplay()) :
+	(displayValue = '', updateDisplay())
+}
 let clearDisplay = () => (displayValue = 0, updateDisplay())
-let delLast = () => displayValue.length > 1 
-	? displayValue = displayValue.slice(0,-1) 
-	: displayValue = 0
-	updateDisplay()
-
+let delLast = () => {displayValue.length > 1 ?
+	(displayValue = displayValue.slice(0,-1)) :
+	(displayValue = 0, updateDisplay())
+}
 let addOper = (a, oper) => {
 	a = displayValue;
 	oper = document.getElementByGroup('oper').value
@@ -38,7 +47,7 @@ let addOper = (a, oper) => {
 
 
 const numBtns = document.querySelectorAll('.num');
-numBtns.forEach(button => (button.addEventListener('click', addToDisplay)));
+numBtns.forEach(button => (button.addEventListener('click', addDisplay)));
 
-const operBtns = docuemnt.querySelectorAll('.oper');
+const operBtns = document.querySelectorAll('.oper');
 operBtns.forEach(button => (button.addEventListener('click'), addOper));
