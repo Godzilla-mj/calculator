40243725cd43d8cc24643c5e794699ec24479e3a
Myung <god.godzilla.af@gmail.com>
2020-01-11 13:18:28 -0500

    display functions working - WIP button event listeners

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 //mathematical functions
 let add = (a,b) => a+b
 let subtract = (a,b) => a-b
@@ -14,6 +15,7 @@ let operate = (a, oper, b) => {
 	if (oper == 'divide');
 		return(divide(a,b));
 }
+
 //num storage
 let display = document.querySelector("#display");
 let displayValue = 0;
@@ -22,17 +24,20 @@ let oper = "";
 let b = 0;
 let tempResult = operate(oper, a, b);
 
+//display functions
 let updateDisplay = () => {
 	display.textContent = displayValue
 };
-let addDisplay = () => {!0 ?
-	(displayValue += this.value, updateDisplay()) :
-	(displayValue = '', updateDisplay())
+
+let addDisplay = () => {displayValue != 0 ?
+	displayValue += this.value : displayValue = '';
+	updateDisplay()
 }
+
 let clearDisplay = () => (displayValue = 0, updateDisplay())
 let delLast = () => {displayValue.length > 1 ?
-	(displayValue = displayValue.slice(0,-1)) :
-	(displayValue = 0, updateDisplay())
+	displayValue = displayValue.slice(0,-1) : displayValue = 0;
+	updateDisplay()
 }
 let addOper = (a, oper) => {
 	a = displayValue;
@@ -40,10 +45,14 @@ let addOper = (a, oper) => {
 	clearDisplay()
 }
 
-
-
+//button event listeners
 const numBtns = document.querySelectorAll('.num');
-numBtns.forEach(button => (button.addEventListener('click', addDisplay));
+numBtns.forEach(button =>{
+	button.addEventListener('click', addDisplay);
+	console.log("numBtn clicked")
+});
 
 const operBtns = document.querySelectorAll('.oper');
-operBtns.forEach(button => (button.addEventListener('click'), addOper));
+operBtns.forEach(button =>{
+	button.addEventListener('click', addOper);
+});
